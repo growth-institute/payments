ac884413d79c569b265a7fb0c412f807394fe986
Rodrigo Tejero <rodrigo.tejero@thewebchi.mp>
2018-07-20 14:50:10 -0500

    Added product image to payment form

--- templates/partials/payments/_product.php
+++ templates/partials/payments/_product.php
@@ -1,9 +1,17 @@
 <?php
 	$parsedown = new Parsedown();
 ?>
-
 	<div class="product-info boxfix-vert">
 		<div class="margins">
+
+			<?php
+				$id_attachment = $form->getMeta("product_image");
+				if ($id_attachment):
+					$attachment_image = Attachments::getById($id_attachment);
+			?>
+				<img class="img-responsive product-image" src="<?php echo($attachment_image->url);?>" alt="<?php sanitized_print($form->name); ?>">
+			<?php endif; ?>
+
 			<div class="the-content">
 				<h1 class="section-title"><?php sanitized_print($form->name); ?></h1>
 				<?php echo $parsedown->text( get_item($form->metas, 'product_description') ); ?>
