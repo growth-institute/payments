dc658231e64793740e7e084bfe7255768406ca2c
Miguel <mig2190@hotmail.com>
2019-03-27 10:11:28 -0600

    added tag manager on payment forms

--- templates/partials/payments/_header-html.php
+++ templates/partials/payments/_header-html.php
@@ -12,5 +12,12 @@
 	<link rel="apple-touch-icon" sizes="114x114" href="<?php $site->img('branding/favicon-lg.png'); ?>" />
 	<!-- Stylesheets -->
 	<?php $site->includeStyles(); ?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KTS6H8');</script>
+	<!-- End Google Tag Manager -->
 </head>
 <body class="<?php $site->bodyClass() ?>">
