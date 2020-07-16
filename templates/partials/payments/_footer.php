efb1877f050454a129b64e93423bd2fa63ef5663
mig21010 <mig2190@hotmail.com>
2020-07-16 09:09:43 -0500

    agregamos links de footer en spanish

--- templates/partials/payments/_footer.php
+++ templates/partials/payments/_footer.php
@@ -3,13 +3,13 @@
 			<div class="margins-horz">
 				<div class="row row-collapse">
 					<div class="col col-6 col-md-6">
-						<p class="legal">&copy; Copyright <?php echo date('Y'); ?>. Gazelles Growth Institute / <a href="https://www.growthinstitute.com/privacy-policy/" target="_blank"><?php $i18n->translate('footer.a-policy'); ?></a> / <a href="https://www.growthinstitute.com/terms-of-use/" target="_blank"><?php $i18n->translate('footer.a-terms'); ?></a></p>
+						<p class="legal">&copy; Copyright <?php echo date('Y'); ?>. Gazelles Growth Institute / <a href="https://www.growthinstitute.com/es/aviso-de-privacidad/" target="_blank"><?php $i18n->translate('footer.a-policy'); ?></a> / <a href="https://www.growthinstitute.com/es/terms-of-use/" target="_blank"><?php $i18n->translate('footer.a-terms'); ?></a></p>
 					</div>
 					<div class="col col-6 col-md-6">
 						<ul class="footer-menu">
-							<li><a href="https://www.growthinstitute.com/about-us/" target="_blank"><?php $i18n->translate('footer.a-about'); ?></a></li>
-							<li><a href="https://blog.growthinstitute.com/" target="_blank"><?php $i18n->translate('footer.a-blog'); ?></a></li>
-							<li><a href="https://www.growthinstitute.com/com/contact-us" target="_blank"><?php $i18n->translate('footer.a-contact'); ?></a></li>
+							<li><a href="https://www.growthinstitute.com/es/about-us/" target="_blank"><?php $i18n->translate('footer.a-about'); ?></a></li>
+							<li><a href="https://blog.growthinstitute.com/es?_ga=2.217169491.419757682.1594740936-1371290148.1594740936" target="_blank"><?php $i18n->translate('footer.a-blog'); ?></a></li>
+							<li><a href="https://www.growthinstitute.com/es/contactanos/" target="_blank"><?php $i18n->translate('footer.a-contact'); ?></a></li>
 							<li><a href="https://www.facebook.com/GrowthInstitute" target="_blank"><i class="fa fa-facebook"></i></a></li>
 							<li><a href="https://twitter.com/ScaleUpWithGI" target="_blank"><i class="fa fa-twitter"></i></a></li>
 							<li><a href="https://www.linkedin.com/school/2736167/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
